9e590d289aa09c0aa5dba50f9bf85c30918088b4
x404 <8bits.softs@gmail.com>
2025-06-18 01:37:59 -0400

    Initial Commit

---
+++ src/main/java/com/x404dev/astralQOL/AstralQOL.java
@@ -0,0 +1,17 @@
+package com.x404dev.astralQOL;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AstralQOL extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
